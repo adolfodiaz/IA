25305f168acc6005341bb4423bf7304a883d14a0
adolfodiaz <adolfo.diaz@usach.cl>
2014-04-23 19:44:39 -0400

    dejando la grande entre el armando y el adolfo

--- IA/app/api/api.js
+++ IA/app/api/api.js
@@ -5,13 +5,17 @@ var Player = require('./Player.js').Player;
 var Match = require('./Match.js').Match;
 var crypto = require('crypto');
 var DB = require('./db/DB.js').DB;
+var Q = require('q');
+
+
+
 db = new DB();
-onlineUsersList = new Object();
-listIdAndUserName = new Object();
+onlineUsersList = new OC();
+listIdAndUserName = new OC();
 
 function api(){
 	
-	this.matchesList = new Object();	
+	this.matchesList = new OC();	
 
 	var mentira = new Match();
 	mentira.name="partida1";
@@ -41,6 +45,8 @@ function api(){
 	}
 
 	this.getListRoundsAndMatchesList = function(req, showTemplate){		
+		var funcionAplazada = Q.defer();
+
 		var list = new Array();
 		for(var round in this.roundsList){
 			list.push(this.roundsList[round]);
@@ -49,132 +55,222 @@ function api(){
 			list.push(this.matchesList[match]);
 		}
 		showTemplate(onlineUsersList[req.user.fullname], Array.prototype.slice.call(list));
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
 
-	this.response = function(connection, data, clientType, response, sendMessage){
-		this.onlineUsersList[data.arguments.clientName] = data.arguments.clientName;
-		sendMessage(connection, clientType, response);
-	}
 	this.prueba = function(){
 		return "prueba";
 	}
 
-	this.probe = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.probe = function(OC){
+		var funcionAplazada = Q.defer();
+		console.log("probe");
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.protocol = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.protocol = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.proto_use_ok = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.proto_use_ok = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.register = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.register = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.reg_sucess = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.reg_sucess = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
 
-	this.session_start = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.session_start = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
 
-	this.accept = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.accept = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
 
-	this.session_quit = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.session_quit = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
 
-	this.stats_query = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.stats_query = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
 
-	this.match_req_info = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.match_req_info = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
 
-	this.match_lookup = function(connection, data, clientType, sendMessage){
-		if(typeof this.matchesList[data.arguments.matchName] === "undefined"){
+	this.match_lookup = function(OC){
+		var funcionAplazada = Q.defer();
+
+		if(typeof this.matchesList[OC.data.arguments.matchName] === "undefined"){
 			//crea la partida y no envia nada al jugador
 			var player1 = new Player();
-			console.log("1");
-			player1.newPlayer(data.arguments.matchName, clientType, connection);
-			console.log("2");
-			this.matchesList[data.arguments.matchName] = new Match();
-			console.log("3");
-			this.matchesList[data.arguments.matchName].newMatch(data.arguments.matchName, listIdAndUserName[data.arguments.id]);
-			console.log("partida creada");
+			player1.newPlayer(OC.data.arguments.matchName, clientType, connection);
+			this.matchesList[OC.data.arguments.matchName] = new Match();
+			this.matchesList[OC.data.arguments.matchName].newMatch(OC.data.arguments.matchName, listIdAndUserName[OC.data.arguments.id]);
 		}else{
 
 		}
-		console.log(this.matchesList[data.arguments.matchName]);
-		sendMessage(connection, clientType, "");
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
 
-	this.match_lookup_cancel = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.match_lookup_cancel = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.match_ready = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.match_ready = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.match_reject = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.match_reject = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.round_start_ack = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.round_start_ack = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.turn_end = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.turn_end = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.turn_query = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.turn_query = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.clock_req = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.clock_req = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.board_check = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.board_check = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.board_req = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.board_req = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.pass = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.pass = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.retire_round = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.retire_round = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.retire_match = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.retire_match = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.projected_tie = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.projected_tie = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.projected_tie_deact = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.projected_tie_deact = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.err_unknown_command = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.err_unknown_command = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.err_args = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.err_args = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.panic_quit = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.panic_quit = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.wait = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.wait = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.resume = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.resume = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.ping = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.ping = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
-	this.pong = function(connection, data, clientType, sendMessage){
-		sendMessage(connection, clientType, response);
+	this.pong = function(OC){
+		var funcionAplazada = Q.defer();
+
+		funcionAplazada.resolve(OC, "");
+		return funcionAplazada.promise;
 	}
 
 
